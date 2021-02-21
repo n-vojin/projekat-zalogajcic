09c67466d1989e7e86fd99313fe3d302f88324fc
Jelena <serjelena9@gmail.com>
2021-02-21 22:12:08 +0100

    Stranica- O nama
    Uradjena stranica O nama(dodat je naslov, tekst, mapa, i lokacija), izmenjen je footer.

--- seminarski/strana1.js
+++ seminarski/strana1.js
@@ -13,20 +13,20 @@ function animateValue(obj, start, end, duration) {
 
 //dodavanje id 
 const obj = document.getElementById("animacijaBr1");
-animateValue(obj, 0, 3, 2000);
+animateValue(obj, 0, 3, 4000);
 
 const obj2 = document.getElementById("animacijaBr2");
-animateValue(obj2, 0, 15, 2000);
+animateValue(obj2, 0, 15, 4000);
 
 const obj3 = document.getElementById("animacijaBr3");
-animateValue(obj3, 0, 1500, 2000);
+animateValue(obj3, 0, 1500, 4000);
 
 const obj4 = document.getElementById("animacijaBr4");
-animateValue(obj4, 0, 10, 1000);
+animateValue(obj4, 0, 10, 2000);
 
 const obj5= document.getElementById("animacijaBr5");
-animateValue(obj5, 0, 1000, 2000);
+animateValue(obj5, 0, 1000, 4000);
 
 const obj6 = document.getElementById("animacijaBr6");
-animateValue(obj6, 0, 40, 2000);
+animateValue(obj6, 0, 40, 4000);
 
